8efa9c1be503a48ab5cc3f68fa6d4d3f416670bf
Pdiya13 <pdiya2945@gmail.com>
2026-01-12 00:38:00 +0530

    add user-service : profile and KYC

---
+++ User-service/src/main/java/com/FinBridgeAA/user_service/Controller/InternalUserController.java
@@ -0,0 +1,27 @@
+package com.FinBridgeAA.user_service.Controller;
+
+import com.FinBridgeAA.user_service.Service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/internal/user")
+@RequiredArgsConstructor
+public class InternalUserController {
+
+    private final UserService service;
+
+    @PostMapping("/create")
+    public ResponseEntity<String> createUser(
+            @RequestHeader("X-USER-ID") UUID userId
+    ) {
+        service.createUserIfNotExists(userId);
+        return ResponseEntity.ok("User created");
+    }
+}

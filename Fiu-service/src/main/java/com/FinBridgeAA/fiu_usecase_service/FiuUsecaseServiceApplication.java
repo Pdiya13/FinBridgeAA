6615d7bc1387572f589345fbb620178df4b0cc1a
Pdiya13 <pdiya2945@gmail.com>
2026-01-15 21:55:57 +0530

    add financial information user service

---
+++ Fiu-service/src/main/java/com/FinBridgeAA/fiu_usecase_service/FiuUsecaseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.FinBridgeAA.fiu_usecase_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiuUsecaseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiuUsecaseServiceApplication.class, args);
+	}
+
+}
